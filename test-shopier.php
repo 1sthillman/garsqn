e3030e1bf454d2246133e27d5d7fd018d9daf93d
Example User <example@example.com>
2025-07-21 16:27:36 +0300

    Shopier entegrasyonu tamamen düzeltildi - api_pay.php endpoint'i kullanılıyor, signature oluşturma düzeltildi, platform parametresi eklendi

--- test-shopier.php
+++ test-shopier.php
@@ -2,16 +2,21 @@
 // test-shopier.php
 $api_key   = 'ba946ce45c717d982cc6decbcb616bb2';
 $secret    = '8a186b6f15655c57c65ec415ae121e69';
-$endpoint  = 'https://www.shopier.com/ShowProduct/api_pay4.php';
+$endpoint  = 'https://www.shopier.com/ShowProduct/api_pay.php';
 
 $payload = [
     'API_key' => $api_key,
     'random_nr' => rand(100000, 999999),
     'platform_order_id' => 999,
     'total_order_value' => 10.00,
+    'currency' => '0', // TRY
+    'website_index' => '1',
+    'platform' => '1', // WooCommerce
+    'is_in_frame' => '0',
+    'current_language' => '0', // TR
 ];
 
-$data = $payload['random_nr'] . $payload['platform_order_id'] . $payload['total_order_value'] . '0';
+$data = $payload['random_nr'] . $payload['platform_order_id'] . $payload['total_order_value'] . $payload['currency'];
 $signature = base64_encode(hash_hmac('sha256', $data, $secret, true));
 $payload['signature'] = $signature;
 
@@ -25,7 +30,11 @@ curl_setopt_array($ch, [
 ]);
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$error = curl_error($ch);
 curl_close($ch);
 
 echo "HTTP Code: $httpCode\n";
-echo "Body: $response\n"; 
+if ($error) {
+    echo "Error: $error\n";
+}
+echo "Body: " . substr($response, 0, 1000) . "...\n"; 
